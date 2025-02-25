91f9dc8c937da43520590bd64befcce556d02409
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-02-25 20:32:03 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.jsx'
-import Course from './components/Course/Course.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
