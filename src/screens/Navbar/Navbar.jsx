755905ada5e9104a639db4577efef1fc57d39dc1
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-02-27 20:47:33 +0530

    nested route pages

--- src/screens/Navbar/Navbar.jsx
+++ src/screens/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <>
-      <nav>
+      <nav className='navbar'>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/dashboard">Dashboard</Link></li>
